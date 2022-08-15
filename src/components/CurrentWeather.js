1eb1f1f4776e6c2aefb49e98c1b19b266f2f6bb0
Christine Lee <christineleehj@gmail.com>
2022-08-15 09:15:31 -0400

    city buttons for ios, alt updated

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -1,12 +1,12 @@
 import { iconCode } from "../data/WeatherData";
 import { WeatherIcons } from "../data/WeatherData";
 
-function CurrentWeather({weather: {details, name, icon, temp }}) {
+function CurrentWeather({weather: {details, icon, temp }}) {
     return (
         <div className="currWeather">
            <h2>Today</h2>
            <div className="currWeather__details">
-                <img src={WeatherIcons[iconCode(icon)]} alt="" />
+                <img src={WeatherIcons[iconCode(icon)]} alt={`${details}`} />
                 <div>
                     <h1>{`${temp.toFixed()}°`}</h1>
                     <h2>{`${details}`}</h2>
